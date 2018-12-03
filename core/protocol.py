221f0eb1659526768988fe6db3893c116da53d0d
Gregfili <gregfili@mail.ru>
2018-12-03 16:40:11 +0300

    Added message type

--- core/protocol.py
+++ core/protocol.py
@@ -2,11 +2,15 @@
 JSON
 Example msg
 {
-msgType: 1, /* 1-econom */
-msgEconType: 1, /* offer */
+v: 1, /*version=1*/
+mt: 1, /* message type 1-offer */
 of: address,
 toType: 1, /* One person */
 to: address,
 
 }
 '''
+mtOffer = 1
+mtOfferAcept = 2
+mtOfferReject = 3
+mtOfferWithdraw = 4
